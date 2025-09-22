bbc265ed065f240f549fc67bea25fc6da51f4db3
Alexander <begemott4@gmail.com>
2025-09-22 16:33:39 +0300

    add workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   typescript: { strict: true },
+  ssr: true,
   app: {
     baseURL: '/luis/',
     head: {
